23774393cf96f68c8637a2ac1ddd978461907d1b
Krystian Plonski <szeejk@gmail.com>
2012-12-10 08:44:11 -0500

    small fix

--- arch/arm/mach-tegra/board-olympus.c
+++ arch/arm/mach-tegra/board-olympus.c
@@ -568,6 +568,7 @@ MACHINE_START(OLYMPUS, "Olympus")
     .boot_params  = 0x00000100,
     .fixup        = mot_fixup,
     .map_io       = tegra_map_common_io,
+    .init_early	  = tegra_init_early,
     .init_irq     = tegra_init_irq,
     .init_machine = tegra_mot_init,
     .timer        = &tegra_timer,
